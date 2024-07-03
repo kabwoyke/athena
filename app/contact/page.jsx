bf8bd217193ad498298c188b5218138856c41fdf
kabwoyke <edwardkabwoy@gmail.com>
2024-07-03 11:56:24 +0300

    with margin top in contact us page adjusted

--- app/contact/page.jsx
+++ app/contact/page.jsx
@@ -1,6 +1,6 @@
 const Contact = () => {
     return (
-        <div className='mt-10'>
+        <div className='mt-20'>
             <section class="bg-blue-50 dark:bg-slate-800" id="contact">
                 <div class="mx-auto max-w-7xl px-4 py-16 sm:px-6 lg:px-8 lg:py-20">
                     <div class="mb-4">
